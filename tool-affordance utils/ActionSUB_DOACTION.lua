cbb5f4dc02dad50a16d790c3cce434f38b2ae9c3
Michele Colledanchise <michele.colledanchise@iit.it>
2017-10-25 12:17:56 +0200

    do action modified, probably the error was in the return value

--- tool-affordance utils/ActionSUB_DOACTION.lua
+++ tool-affordance utils/ActionSUB_DOACTION.lua
@@ -3,10 +3,9 @@ print("Performing action ", action)
 local actOK = perform_action(action, target_object)
 if actOK then
   print("Action Performed: ", action)
-  -- state = "comp_effect"
+  --state = "comp_effect"
   state = "observe"
  else
   print("Action ", action, "could not be executed")
 end
-return success
-is_action_done = false
+return true
