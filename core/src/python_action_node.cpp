069d13d34431d39cf63340721a8c6cd67484b2b2
mcolledanchise <miccol@kth.se>
2018-04-12 16:33:46 +0200

    added Py_DECREF in the proper places

--- core/src/python_action_node.cpp
+++ core/src/python_action_node.cpp
@@ -1,49 +1,48 @@
 #include <python_action_node.h>
 #include <Python.h>
 
-PyObject *python_state_;
+PyObject *python_state_,*python_state_2;
 PyObject *python_tick_fn_, *python_halt_fn_, *python_finalize_fn_; //TODO Figure out why if It cannot find Python.h in the header
 
 static PyObject *
 SetValueOnBlackboard(PyObject *self, PyObject *args)
 {
+
+    const char *key;
+
     int int_value;
     const char *str_value;
     float float_value;
+    bool bool_value;
 
-    const char *passed;
-    int value = 1;
 
-    // bool args_ok = PyArg_ParseTuple(args, "si", &passed, &int_value);
-
-    if (PyArg_ParseTuple(args, "si", &passed, &int_value))
+    if (PyArg_ParseTuple(args, "si", &key, &int_value))
     {
-        std::cout << "*****************Setting Value int **********************" << passed << int_value << std::endl;
-            Py_DECREF(int_value);
+        std::cout << "*****************Setting Value int **********************" << key << int_value << std::endl;
     }
-    else if(PyArg_ParseTuple(args, "sf", &passed, &float_value))
+    else if (PyArg_ParseTuple(args, "sf", &key, &float_value))
     {
-        std::cout << "*****************Setting Value float**********************" << passed << float_value << std::endl;
-           // Py_DECREF(float_value);
-
+        std::cout << "*****************Setting Value float**********************" << key << float(float_value) << std::endl;
     }
 
-    else if(PyArg_ParseTuple(args, "ss", &passed, &str_value))
+    else if (PyArg_ParseTuple(args, "ss", &key, &str_value))
     {
-        std::cout << "*****************Setting Value str **********************" << passed << str_value << std::endl;
-    Py_DECREF(str_value);
+        std::cout << "*****************Setting Value str **********************" << key << str_value << std::endl;
+    }
+        else if (PyArg_ParseTuple(args, "sp", &key, &bool_value))
+    {
+        std::cout << "*****************Setting Value bool **********************" << key << bool_value << std::endl;
     }
     else
     {
         std::cout << " Invalid Argument Passed " << std::endl;
+        return NULL;
     }
-        std::cout << " Returning " << std::endl;
-
-
-            Py_DECREF(args);
-
-    return NULL;
-    //return PyLong_FromLong(value);
+    std::cout << " Returning " << std::endl;
+     Py_DECREF(args);
+     Py_DECREF(self);
+//return PyLong_FromLong(1);
+    Py_RETURN_NONE;
 }
 
 static PyObject *
@@ -83,6 +82,7 @@ BT::PythonActionNode::PythonActionNode(std::string name, std::string filename, y
 
     Py_Initialize();
     python_state_ = PyImport_ImportModule((char *)cstr);
+    python_state_2 = PyImport_ImportModule((char *)cstr);
 
     if (!python_state_)
     {
@@ -91,7 +91,7 @@ BT::PythonActionNode::PythonActionNode(std::string name, std::string filename, y
 
     // creating the PyObjects related to the functions in the python script.
 
-    PyObject *python_init_fn = PyObject_GetAttrString(python_state_, (char *)"init");
+    PyObject *python_init_fn = PyObject_GetAttrString(python_state_2, (char *)"init");
     python_tick_fn_ = PyObject_GetAttrString(python_state_, (char *)"tick");
     python_halt_fn_ = PyObject_GetAttrString(python_state_, (char *)"halt");
     python_finalize_fn_ = PyObject_GetAttrString(python_state_, (char *)"finalize");
@@ -104,9 +104,11 @@ BT::PythonActionNode::PythonActionNode(std::string name, std::string filename, y
     PyObject *topass = PyModule_Create(&blackboard_module);
 
     //PyObject* topass = PyFloat_FromDouble(2.0);
-    PyObject *empty_args = PyTuple_New(0);
+    //PyObject *empty_args = PyTuple_New(0);
     PyObject *args = PyTuple_Pack(1, topass);
-    PyObject *python_result = PyObject_CallObject(python_init_fn, args);
+    PyObject_CallObject(python_init_fn, args);
+    Py_DECREF(args);
+
     //Py_DECREF(args);                // used for managing reference counts of Python objects.
     Py_DECREF(python_init_fn);      // used for managing reference counts of Python objects.
     Py_DECREF(python_tick_fn_);     // used for managing reference counts of Python objects.
@@ -132,13 +134,18 @@ BT::ReturnStatus BT::PythonActionNode::Tick()
     set_status(BT::RUNNING);
     // calling the function tick in the python script with empty argument
     PyObject *empty_args = PyTuple_New(0);
+    
     PyObject *python_result = PyObject_CallObject(python_tick_fn_, empty_args);
+Py_DECREF(empty_args);
+
+    std::cout << "I am fooling you" << std::endl;
     //Py_DECREF(empty_args);      // used for managing reference counts of Python objects.
 
     // parsing the final return from the python script. The python script has to return True (Success) or False (Failure).
     // The Running status is taken for granted while running the script
 
-    bool has_succeeded = PyObject_IsTrue(python_result);
+    bool has_succeeded = true; PyObject_IsTrue(python_result);
+//Py_DECREF(python_result);
 
     if (has_succeeded)
     {
@@ -156,17 +163,17 @@ void BT::PythonActionNode::Halt()
 {
     // calling the function finalize in the python script with empty argument
     PyObject *empty_args = PyTuple_New(0);
-    PyObject *python_result = PyObject_CallObject(python_halt_fn_, empty_args);
+    PyObject_CallObject(python_halt_fn_, empty_args);
 
-    //Py_DECREF(empty_args);      // used for managing reference counts of Python objects.
+    Py_DECREF(empty_args);      // used for managing reference counts of Python objects.
 }
 void BT::PythonActionNode::Finalize()
 {
     // calling the function finalize in the python script with empty argument
     PyObject *empty_args = PyTuple_New(0);
-    PyObject *python_result = PyObject_CallObject(python_finalize_fn_, empty_args);
+    PyObject_CallObject(python_finalize_fn_, empty_args);
 
-    //Py_DECREF(empty_args);          // used for managing reference counts of Python objects.
+    Py_DECREF(empty_args);          // used for managing reference counts of Python objects.
 
     // Finalizing the python api
     Py_Finalize();
